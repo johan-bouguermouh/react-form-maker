c6f35fd93f8c07118c46d329da52597aff070ea7
johan-bouguermouh <91847583+johan-bouguermouh@users.noreply.github.com>
2025-06-13 14:35:23 +0200

    Factor: no-unused-vars, no-explicit-any

--- frontend/src/components/ReactFormMaker/interfaces/CustomInputFieldElementParams.ts
+++ frontend/src/components/ReactFormMaker/interfaces/CustomInputFieldElementParams.ts
@@ -1,8 +1,8 @@
 import { ControllerRenderProps, FieldValues, Path } from 'react-hook-form';
 import { FieldReactFormMaker } from './FieldInterfaces';
 
-export interface CustomInputFieldElementParams<T> {
-  zFields?: ControllerRenderProps<any, Path<any>>;
+export interface CustomInputFieldElementParams<T extends FieldValues> {
+  zFields?: ControllerRenderProps<T, Path<T>>;
   fieldProps?: FieldReactFormMaker;
   index?: string;
   props?: T;
