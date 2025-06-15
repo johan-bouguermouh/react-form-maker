36a6ce662b71185e2c032a32af38f44a89cbe852
johan-bouguermouh <91847583+johan-bouguermouh@users.noreply.github.com>
2025-06-15 15:45:05 +0200

    Refactor SelectSimple component to improve readability and maintainability; update value handling and merge refs correctly.

--- frontend/src/components/ReactFormMaker/enhancements/SelectSimple.tsx
+++ frontend/src/components/ReactFormMaker/enhancements/SelectSimple.tsx
@@ -8,9 +8,10 @@ import {
   SelectLabel,
   SelectItem,
 } from '@/components/ui/select';
+import * as SelectPrimitive from '@radix-ui/react-select';
 import { FieldParams } from '../interfaces/FieldParams';
 import { isOption } from '../utils/typeGuards/optionsFields.TypeGuards';
-import * as SelectPrimitive from '@radix-ui/react-select';
+import { mergeRefs } from '@/lib/utils';
 
 const SelectSimple = React.forwardRef<
   React.ElementRef<typeof SelectPrimitive.Trigger>, // Type attendu par SelectTrigger
@@ -18,39 +19,46 @@ const SelectSimple = React.forwardRef<
 >(
   (
     { zFields, fieldProps, indexField }: FieldParams,
-    ref: React.ForwardedRef<any>,
-  ) => (
-    <Select
-      key={indexField}
-      //className={fieldProps.className}
-      disabled={fieldProps.disabled}
-      onValueChange={(value: string | number) => {
-        //si la valeut est un string numeraire alors on le change en number
-        if (typeof value === 'string' && !isNaN(Number(value))) {
-          value = Number(value);
-        }
-        zFields.onChange(value);
-      }}
-      {...zFields}
-    >
-      <SelectTrigger ref={ref}>
-        <SelectValue placeholder={fieldProps.placeholder} />
-      </SelectTrigger>
-      <SelectContent>
-        <SelectGroup>
-          <SelectLabel>{fieldProps.label}</SelectLabel>
-          {fieldProps.options?.map((option, index) => (
-            <SelectItem
-              key={isOption(option) ? 'select-item-' + option.value : option}
-              value={isOption(option) ? (option.value as string) : option}
-            >
-              {isOption(option) ? option.label : option}
-            </SelectItem>
-          ))}
-        </SelectGroup>
-      </SelectContent>
-    </Select>
-  ),
+    ref: React.ForwardedRef<React.ElementRef<typeof SelectPrimitive.Trigger>>,
+  ) => {
+    const { ref: zFieldsRef, ...restZFields } = zFields;
+    return (
+      <Select
+        key={indexField}
+        // className={fieldProps.className}
+        disabled={fieldProps.disabled}
+        onValueChange={(value: string | number) => {
+          // si la valeut est un string numeraire alors on le change en number
+          let newValue = value;
+          if (
+            typeof value === 'string' &&
+            Number.isFinite(parseInt(value, 10))
+          ) {
+            newValue = Number(value);
+          }
+          zFields.onChange(newValue);
+        }}
+        {...restZFields}
+      >
+        <SelectTrigger ref={mergeRefs(ref, zFieldsRef)}>
+          <SelectValue placeholder={fieldProps.placeholder} />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            <SelectLabel>{fieldProps.label}</SelectLabel>
+            {fieldProps.options?.map((option) => (
+              <SelectItem
+                key={isOption(option) ? `select-item-${option.value}` : option}
+                value={isOption(option) ? (option.value as string) : option}
+              >
+                {isOption(option) ? option.label : option}
+              </SelectItem>
+            ))}
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+    );
+  },
 );
 SelectSimple.displayName = 'SelectSimple';
 
