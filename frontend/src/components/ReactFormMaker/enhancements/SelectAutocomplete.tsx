ef68fca4e4c35d7eb8aff02b77abfc98be1766db
johan-bouguermouh <91847583+johan-bouguermouh@users.noreply.github.com>
2025-11-14 16:24:28 +0100

    fix: refactor SelectAutocomplete en utilisant React.forwardRef pour une meilleure gestion des références

--- frontend/src/components/ReactFormMaker/enhancements/SelectAutocomplete.tsx
+++ frontend/src/components/ReactFormMaker/enhancements/SelectAutocomplete.tsx
@@ -69,120 +69,137 @@ export interface SelectAutocompleteParams
  * @param param0
  * @returns
  */
-export function SelectAutocomplete({
-  defaultValue,
-  onChange,
-  options,
-  placeholder,
-  legend = 'Select option...',
-  id,
-  ...props
-}: SelectAutocompleteParams) {
-  const [open, setOpen] = React.useState(props.defaultOpen || false);
-  const [value, setValue] = React.useState(defaultValue || null);
-  const [currentOption, setCurrentOption] = React.useState<Option[] | string[]>(
-    options,
-  );
+export const SelectAutocomplete = React.forwardRef<
+  HTMLButtonElement,
+  SelectAutocompleteParams
+>(
+  (
+    {
+      defaultValue,
+      onChange,
+      options,
+      placeholder,
+      legend = 'Select option...',
+      id,
+      ...props
+    },
+    ref,
+  ) => {
+    const [open, setOpen] = React.useState(props.defaultOpen || false);
+    const [value, setValue] = React.useState(defaultValue || null);
+    const [currentOption, setCurrentOption] = React.useState<
+      Option[] | string[]
+    >(options);
 
-  const handleInputChange = (data: string) => {
-    if (!data || data === '') setCurrentOption(options);
-    else if (isOptionsArray(options)) {
-      setCurrentOption(
-        options.filter((optionItem) =>
-          optionItem.label
-            .toLowerCase()
-            .trim()
-            .includes(data.toLowerCase().trim()),
-        ),
-      );
-    } else {
-      setCurrentOption(
-        options.filter((optionItem) =>
-          optionItem.toLowerCase().trim().includes(data.toLowerCase().trim()),
-        ),
-      );
-    }
-  };
+    React.useEffect(() => {
+      onChange(value);
+    }, [value, onChange]);
 
-  return (
-    <Popover open={open} onOpenChange={setOpen}>
-      <PopoverTrigger asChild>
-        <Button
-          variant="outline"
-          role="combobox"
-          aria-expanded={open}
-          className="w-[200px] justify-between"
-          aria-controls={id}
-          aria-haspopup="listbox"
-          aria-owns={id}
-          aria-autocomplete="list"
-          aria-activedescendant={value ? `option-${value}` : undefined}
-          id={id}
-        >
-          {value
-            ? (() => {
-                const selected = options.find((option) =>
-                  isOption(option) ? option.value === value : option === value,
-                );
-                if (!selected) return legend;
-                if (isOption(selected)) return selected.label;
-                if (typeof selected === 'string') return selected;
-                return legend;
-              })()
-            : legend}
-          <CaretSortIcon className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-        </Button>
-      </PopoverTrigger>
-      <PopoverContent className="w-[200px] p-0">
-        <Command>
-          <CommandInput
-            placeholder={placeholder}
-            className="h-9"
-            onValueChange={(inputValue) => handleInputChange(inputValue)}
-          />
-          <CommandList>
-            <CommandEmpty>No option found.</CommandEmpty>
-            <CommandGroup>
-              {currentOption &&
-                currentOption.map((option) => (
-                  <CommandItem
-                    id={`option-${isOption(option) ? option.value : option}`}
-                    key={isOption(option) ? option.value : option}
-                    value={isOption(option) ? option.label : option}
-                    onSelect={(currentValue) => {
-                      const selectedValue = isOptionsArray(options)
-                        ? options.find(
-                            (optionItem) => optionItem.label === currentValue,
-                          )
-                        : options.find(
-                            (optionItem) => optionItem === currentValue,
-                          );
-                      if (!selectedValue) {
-                        return;
-                      }
-                      const extractedValue = isOption(selectedValue)
-                        ? selectedValue.value
-                        : selectedValue;
-                      const isUnselecting = extractedValue === value;
-                      setValue(isUnselecting ? '' : extractedValue);
-                      if (!isUnselecting) setOpen(false);
-                    }}
-                  >
-                    {isOption(option) ? option.label : option}
-                    <CheckIcon
-                      className={cn(
-                        'ml-auto h-4 w-4',
-                        value === (isOption(option) ? option.value : option)
-                          ? 'opacity-100'
-                          : 'opacity-0',
-                      )}
-                    />
-                  </CommandItem>
-                ))}
-            </CommandGroup>
-          </CommandList>
-        </Command>
-      </PopoverContent>
-    </Popover>
-  );
-}
+    const handleInputChange = (data: string) => {
+      if (!data || data === '') setCurrentOption(options);
+      else if (isOptionsArray(options)) {
+        setCurrentOption(
+          options.filter((optionItem) =>
+            optionItem.label
+              .toLowerCase()
+              .trim()
+              .includes(data.toLowerCase().trim()),
+          ),
+        );
+      } else {
+        setCurrentOption(
+          options.filter((optionItem) =>
+            optionItem.toLowerCase().trim().includes(data.toLowerCase().trim()),
+          ),
+        );
+      }
+    };
+
+    return (
+      <Popover open={open} onOpenChange={setOpen}>
+        <PopoverTrigger asChild>
+          <Button
+            ref={ref}
+            variant="outline"
+            role="combobox"
+            aria-expanded={open}
+            className="w-[200px] justify-between"
+            aria-controls={id}
+            aria-haspopup="listbox"
+            aria-owns={id}
+            aria-autocomplete="list"
+            aria-activedescendant={value ? `option-${value}` : undefined}
+            id={id}
+          >
+            {value
+              ? (() => {
+                  const selected = options.find((option) =>
+                    isOption(option)
+                      ? option.value === value
+                      : option === value,
+                  );
+                  if (!selected) return legend;
+                  if (isOption(selected)) return selected.label;
+                  if (typeof selected === 'string') return selected;
+                  return legend;
+                })()
+              : legend}
+            <CaretSortIcon className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-[200px] p-0">
+          <Command>
+            <CommandInput
+              placeholder={placeholder}
+              className="h-9"
+              onValueChange={(inputValue) => handleInputChange(inputValue)}
+            />
+            <CommandList>
+              <CommandEmpty>No option found.</CommandEmpty>
+              <CommandGroup>
+                {currentOption &&
+                  currentOption.map((option) => (
+                    <CommandItem
+                      id={`option-${isOption(option) ? option.value : option}`}
+                      key={isOption(option) ? option.value : option}
+                      value={isOption(option) ? option.label : option}
+                      onSelect={(currentValue) => {
+                        const selectedValue = isOptionsArray(options)
+                          ? options.find(
+                              (optionItem) => optionItem.label === currentValue,
+                            )
+                          : options.find(
+                              (optionItem) => optionItem === currentValue,
+                            );
+                        if (!selectedValue) {
+                          return;
+                        }
+                        const extractedValue = isOption(selectedValue)
+                          ? selectedValue.value
+                          : selectedValue;
+                        const isUnselecting = extractedValue === value;
+                        setValue(isUnselecting ? '' : extractedValue);
+                        if (!isUnselecting) setOpen(false);
+                      }}
+                    >
+                      {isOption(option) ? option.label : option}
+                      <CheckIcon
+                        className={cn(
+                          'ml-auto h-4 w-4',
+                          value === (isOption(option) ? option.value : option)
+                            ? 'opacity-100'
+                            : 'opacity-0',
+                        )}
+                      />
+                    </CommandItem>
+                  ))}
+              </CommandGroup>
+            </CommandList>
+          </Command>
+        </PopoverContent>
+      </Popover>
+    );
+  },
+);
+
+SelectAutocomplete.displayName = 'SelectAutocomplete';
