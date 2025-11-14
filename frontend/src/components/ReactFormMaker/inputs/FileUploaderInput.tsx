c1897104b01ce8adefb039ad78baf7a3049a613e
johan-bouguermouh <91847583+johan-bouguermouh@users.noreply.github.com>
2025-11-14 16:42:25 +0100

    fix: supprimer le code commenté et nettoyer la fonction FileDropZone

--- frontend/src/components/ReactFormMaker/inputs/FileUploaderInput.tsx
+++ frontend/src/components/ReactFormMaker/inputs/FileUploaderInput.tsx
@@ -3,19 +3,16 @@ import type { DropzoneOptions } from 'react-dropzone';
 import type { FieldParams } from '../interfaces/FieldParams';
 import DefaultFileUploader from '../enhancements/FileUploader/variants/DefaultFileUploader';
 
-function isFile(obj: any): boolean {
-  return (
-    obj instanceof File ||
-    Object.prototype.toString.call(obj) === '[object File]'
-  );
-}
+// function isFile(obj: any): boolean {
+//   return (
+//     obj instanceof File ||
+//     Object.prototype.toString.call(obj) === '[object File]'
+//   );
+// }
 
-function FileDropZone({ zFields, fieldProps, indexField }: FieldParams) {
+function FileDropZone({ zFields, fieldProps }: FieldParams) {
+  /* eslint-disable-next-line @typescript-eslint/no-unsafe-assignment */
   const { value, onChange, ...restZfields } = zFields;
-
-  const exempleInstanceOfFileExemple = new File([], 'exemple.txt', {
-    type: 'text/plain',
-  });
 
   const dropzoneOptions: DropzoneOptions = {
     disabled: fieldProps.disabled,
@@ -23,11 +20,13 @@ function FileDropZone({ zFields, fieldProps, indexField }: FieldParams) {
 
   // Filtrer les propriétés inexistantes
   const filteredFieldProps = Object.fromEntries(
+    /* eslint-disable-next-line @typescript-eslint/no-unused-vars */
     Object.entries(fieldProps).filter(([_, v]) => v !== undefined),
   );
 
   return (
     <DefaultFileUploader
+      /* eslint-disable-next-line @typescript-eslint/no-unsafe-assignment */
       value={value}
       onValueChange={onChange}
       {...restZfields}
